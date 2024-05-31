bab8b96d15ae90a25ca78ed8d38b1aa5823fae45
amedeo-pasanisi <amedeopasanisi@gmail.com>
2024-05-31 17:54:57 +0200

    responsive panel, quotes on variables values, formatted date

--- src/components/CalendarioPanel.tsx
+++ src/components/CalendarioPanel.tsx
@@ -7,9 +7,9 @@ const { RangePicker } = DatePicker;
 import { css, cx } from '@emotion/css';
 import dayjs from 'dayjs'
 
-interface Props extends PanelProps<CalendarioOptions> {}
+interface CalendarioPanelProps extends PanelProps<CalendarioOptions> {}
 
-export const CalendarioPanel: React.FC<Props> = ({ options, data, width, height, fieldConfig, id, replaceVariables  }) => {
+export const CalendarioPanel: React.FC<CalendarioPanelProps> = ({ options, data, width, height, fieldConfig, id, replaceVariables  }) => {
   const data_inizio = replaceVariables(`$${options.variable_data_inizio}`);
   const data_fine = replaceVariables(`$${options.variable_data_fine}`);
 
@@ -18,27 +18,30 @@ export const CalendarioPanel: React.FC<Props> = ({ options, data, width, height,
   }
 
   return (
-    <RangePicker
-      className={cx(
-        css`
-          margin-top: 25px;
-          width: ${width}px;
-          height: ${height - 25}px;
-          min-height: 40px;
-          min-width: 170px;
-        `
-      )}
-      size="large"
-      value={[
-        dayjs(data_inizio).isValid() ? dayjs(data_inizio) : undefined,
-        dayjs(data_fine).isValid() ? dayjs(data_fine) : undefined
-      ]}
-      onChange={value => {
-        locationService.partial({
-          [`var-${options.variable_data_inizio}`]: value?.[0]?.format('YYYY-MM-DD') ?? undefined,
-          [`var-${options.variable_data_fine}`]: value?.[1]?.format('YYYY-MM-DD') ?? undefined
-        }, true)}
-      }
-    />
+    <>
+      <RangePicker
+        className={cx(
+          css`
+            margin-top: 25px;
+            width: ${width}px;
+            height: ${height - 25}px;
+            min-height: 40px;
+            min-width: 170px;
+          `
+        )}
+        size="large"
+        format={'DD-MM-YYYY'}
+        value={[
+          dayjs(data_inizio).isValid() ? dayjs(data_inizio) : undefined,
+          dayjs(data_fine).isValid() ? dayjs(data_fine) : undefined
+        ]}
+        onChange={value => {
+          locationService.partial({
+            [`var-${options.variable_data_inizio}`]: value?.[0]?.format('YYYY-MM-DD') ? `"${value?.[0]?.format('YYYY-MM-DD') ?? ''}"` : undefined,
+            [`var-${options.variable_data_fine}`]: value?.[1]?.format('YYYY-MM-DD') ? `"${value?.[1]?.format('YYYY-MM-DD') ?? ''}"` : undefined
+          }, true)}
+        }
+      />
+    </>
   );
 };
